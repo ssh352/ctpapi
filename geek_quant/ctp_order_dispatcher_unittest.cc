a76a1faebc2e3daf15ae725dec9f460d4af3181f
yjqpro <yjqpro@gmail.com>
2017-03-17 10:27:50 +0800

    more unittest

--- geek_quant/ctp_order_dispatcher_unittest.cc
+++ geek_quant/ctp_order_dispatcher_unittest.cc
@@ -4,9 +4,9 @@
 
 CThostFtdcOrderField MakeRtnOrderField(
     TThostFtdcOrderStatusType order_status,
+    TThostFtdcOffsetFlagType oc_flag = THOST_FTDC_OF_Open,
     TThostFtdcVolumeType volume_traded = 0,
     TThostFtdcVolumeType volume_total = 10,
-    TThostFtdcOffsetFlagType oc_flag = THOST_FTDC_OF_Open,
     TThostFtdcDirectionType direction = THOST_FTDC_D_Buy,
     TThostFtdcOrderRefType order_no = "00001",
     TThostFtdcPriceType limit_price = 1234.1,
@@ -49,25 +49,108 @@ TEST(CtpOrderDispatcherTest, OpenOrder) {
   }
 
   {
-    auto order = dispatcher.HandleRtnOrder(
-        MakeRtnOrderField(THOST_FTDC_OST_PartTradedQueueing, 1, 9));
+    auto order = dispatcher.HandleRtnOrder(MakeRtnOrderField(
+        THOST_FTDC_OST_PartTradedQueueing, THOST_FTDC_OF_Open, 1, 9));
     EXPECT_TRUE(order);
     EXPECT_EQ(1, order->volume);
     EXPECT_EQ(kOSOpened, order->order_status);
   }
 
   {
-    auto order = dispatcher.HandleRtnOrder(
-        MakeRtnOrderField(THOST_FTDC_OST_PartTradedQueueing, 5, 5));
+    auto order = dispatcher.HandleRtnOrder(MakeRtnOrderField(
+        THOST_FTDC_OST_PartTradedQueueing, THOST_FTDC_OF_Open, 5, 5));
     EXPECT_TRUE(order);
     EXPECT_EQ(4, order->volume);
     EXPECT_EQ(kOSOpened, order->order_status);
   }
   {
     auto order = dispatcher.HandleRtnOrder(
-        MakeRtnOrderField(THOST_FTDC_OST_AllTraded, 10, 0));
+        MakeRtnOrderField(THOST_FTDC_OST_AllTraded, THOST_FTDC_OF_Open, 10, 0));
     EXPECT_TRUE(order);
     EXPECT_EQ(5, order->volume);
     EXPECT_EQ(kOSOpened, order->order_status);
+  }
+}
+
+TEST(CtpOrderDispatcherTest, CloseOrder) {
+  CtpOrderDispatcher dispatcher;
+  {
+    auto order = dispatcher.HandleRtnOrder(
+        MakeRtnOrderField(THOST_FTDC_OST_Unknown, THOST_FTDC_OF_Close));
+    EXPECT_TRUE(order);
+    EXPECT_EQ("abc", order->instrument);
+    EXPECT_EQ("00001", order->order_no);
+    EXPECT_EQ(kOSCloseing, order->order_status);
+    EXPECT_EQ(1234.1, order->order_price);
+    EXPECT_EQ(10, order->volume);
+  }
+
+  {
+    auto order = dispatcher.HandleRtnOrder(
+        MakeRtnOrderField(THOST_FTDC_OST_NoTradeQueueing, THOST_FTDC_OF_Close));
+    EXPECT_FALSE(order);
+  }
+
+  {
+    auto order = dispatcher.HandleRtnOrder(
+        MakeRtnOrderField(THOST_FTDC_OST_NoTradeQueueing, THOST_FTDC_OF_Close));
+    EXPECT_FALSE(order);
+  }
+
+  {
+    auto order = dispatcher.HandleRtnOrder(MakeRtnOrderField(
+        THOST_FTDC_OST_PartTradedQueueing, THOST_FTDC_OF_Close, 1, 9));
+    EXPECT_TRUE(order);
+    EXPECT_EQ(1, order->volume);
+    EXPECT_EQ(kOSClosed, order->order_status);
+  }
+
+  {
+    auto order = dispatcher.HandleRtnOrder(MakeRtnOrderField(
+        THOST_FTDC_OST_PartTradedQueueing, THOST_FTDC_OF_Close, 5, 5));
+    EXPECT_TRUE(order);
+    EXPECT_EQ(4, order->volume);
+    EXPECT_EQ(kOSClosed, order->order_status);
+  }
+  {
+    auto order = dispatcher.HandleRtnOrder(MakeRtnOrderField(
+        THOST_FTDC_OST_AllTraded, THOST_FTDC_OF_Close, 10, 0));
+    EXPECT_TRUE(order);
+    EXPECT_EQ(5, order->volume);
+    EXPECT_EQ(kOSClosed, order->order_status);
+  }
+}
+// THOST_FTDC_OST_Canceled
+
+TEST(CtpOrderDispatcherTest, CancelOrder) {
+  CtpOrderDispatcher dispatcher;
+  {
+    auto order =
+        dispatcher.HandleRtnOrder(MakeRtnOrderField(THOST_FTDC_OST_Unknown));
+    EXPECT_TRUE(order);
+    EXPECT_EQ("abc", order->instrument);
+    EXPECT_EQ("00001", order->order_no);
+    EXPECT_EQ(kOSOpening, order->order_status);
+    EXPECT_EQ(1234.1, order->order_price);
+    EXPECT_EQ(10, order->volume);
+  }
+
+  {
+    auto order = dispatcher.HandleRtnOrder(
+        MakeRtnOrderField(THOST_FTDC_OST_NoTradeQueueing));
+    EXPECT_FALSE(order);
+  }
+
+  {
+    auto order = dispatcher.HandleRtnOrder(
+        MakeRtnOrderField(THOST_FTDC_OST_NoTradeQueueing));
+    EXPECT_FALSE(order);
+  }
+
+  {
+    auto order =
+        dispatcher.HandleRtnOrder(MakeRtnOrderField(THOST_FTDC_OST_Canceled));
+    EXPECT_TRUE(order);
+    EXPECT_EQ("00001", order->order_no);
   }
 }
