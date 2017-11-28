368800d7865e0ef195964a7c0e09b0ee5a8ef556
yjqpro <yjqpro@gmail.com>
2017-11-28 14:33:20 +0800

    format code

--- live_trade/sub_account_broker.cc
+++ live_trade/sub_account_broker.cc
@@ -15,72 +15,68 @@ std::unordered_map<std::string, std::string> g_instrument_exchange_set = {
 std::unordered_set<std::string> g_close_today_cost_instrument_codes = {
     "fg", "i", "j", "jm", "ma", "ni", "pb", "rb", "sf", "sm", "zc", "zn"};
 
-
-std::string CAFSubAccountBroker::GenerateOrderId()
-{
+std::string CAFSubAccountBroker::GenerateOrderId() {
   return boost::lexical_cast<std::string>(order_seq_++);
 }
 
-void CAFSubAccountBroker::ReturnOrderField(const std::shared_ptr<OrderField>& order)
-{
+void CAFSubAccountBroker::ReturnOrderField(
+    const std::shared_ptr<OrderField>& order) {
   inner_mail_box_->Send(order);
 }
 
-void CAFSubAccountBroker::HandleCancelOrder(const CTPCancelOrder& cancel_order)
-{
+void CAFSubAccountBroker::HandleCancelOrder(
+    const CTPCancelOrder& cancel_order) {
   common_mail_box_->Send(account_id_, cancel_order);
 }
 
-void CAFSubAccountBroker::HandleEnterOrder(const CTPEnterOrder& enter_order)
-{
+void CAFSubAccountBroker::HandleEnterOrder(const CTPEnterOrder& enter_order) {
   common_mail_box_->Send(account_id_, enter_order);
 }
 
-void CAFSubAccountBroker::MakeCtpInstrumentBrokerIfNeed(const std::string& instrument)
-{
+void CAFSubAccountBroker::MakeCtpInstrumentBrokerIfNeed(
+    const std::string& instrument) {
   if (instrument_brokers_.find(instrument) != instrument_brokers_.end()) {
     return;
   }
   auto broker = std::make_unique<CTPInstrumentBroker>(
-    this, instrument, false,
-    std::bind(&CAFSubAccountBroker::GenerateOrderId, this));
+      this, instrument, false,
+      std::bind(&CAFSubAccountBroker::GenerateOrderId, this));
   std::string instrument_code =
-    instrument.substr(0, instrument.find_first_of("0123456789"));
+      instrument.substr(0, instrument.find_first_of("0123456789"));
   boost::algorithm::to_lower(instrument_code);
   bool close_today_cost = false;
   bool close_today_aware = false;
   if (g_instrument_exchange_set.find(instrument_code) !=
-    g_instrument_exchange_set.end() &&
-    g_instrument_exchange_set.at(instrument_code) == "sc") {
+          g_instrument_exchange_set.end() &&
+      g_instrument_exchange_set.at(instrument_code) == "sc") {
     close_today_aware = true;
   } else {
   }
 
   if (g_close_today_cost_instrument_codes.find(instrument_code) !=
-    g_close_today_cost_instrument_codes.end()) {
+      g_close_today_cost_instrument_codes.end()) {
     close_today_cost = true;
   }
 
   if (close_today_cost && close_today_aware) {
     broker->SetPositionEffectStrategy<
-      CloseTodayCostCTPPositionEffectStrategy,
-      CloseTodayAwareCTPPositionEffectFlagStrategy>();
+        CloseTodayCostCTPPositionEffectStrategy,
+        CloseTodayAwareCTPPositionEffectFlagStrategy>();
   } else if (!close_today_cost && close_today_aware) {
     broker->SetPositionEffectStrategy<
-      GenericCTPPositionEffectStrategy,
-      CloseTodayAwareCTPPositionEffectFlagStrategy>();
+        GenericCTPPositionEffectStrategy,
+        CloseTodayAwareCTPPositionEffectFlagStrategy>();
   } else if (close_today_cost && !close_today_aware) {
     broker->SetPositionEffectStrategy<CloseTodayCostCTPPositionEffectStrategy,
-      GenericCTPPositionEffectFlagStrategy>();
+                                      GenericCTPPositionEffectFlagStrategy>();
   } else {
     broker->SetPositionEffectStrategy<GenericCTPPositionEffectStrategy,
-      GenericCTPPositionEffectFlagStrategy>();
+                                      GenericCTPPositionEffectFlagStrategy>();
   }
-  instrument_brokers_.insert({ instrument, std::move(broker) });
+  instrument_brokers_.insert({instrument, std::move(broker)});
 }
 
-caf::behavior CAFSubAccountBroker::make_behavior()
-{
+caf::behavior CAFSubAccountBroker::make_behavior() {
   return {
       [=](const std::vector<CTPPositionField>& positions) {
         for (const auto& pos : positions) {
@@ -134,11 +130,14 @@ caf::behavior CAFSubAccountBroker::make_behavior()
   };
 }
 
-CAFSubAccountBroker::CAFSubAccountBroker(caf::actor_config& cfg, LiveTradeMailBox* inner_mail_box, LiveTradeMailBox* common_mail_box, std::string account_id) : caf::event_based_actor(cfg),
-inner_mail_box_(inner_mail_box),
-common_mail_box_(common_mail_box),
-account_id_(std::move(account_id))
-{
+CAFSubAccountBroker::CAFSubAccountBroker(caf::actor_config& cfg,
+                                         LiveTradeMailBox* inner_mail_box,
+                                         LiveTradeMailBox* common_mail_box,
+                                         std::string account_id)
+    : caf::event_based_actor(cfg),
+      inner_mail_box_(inner_mail_box),
+      common_mail_box_(common_mail_box),
+      account_id_(std::move(account_id)) {
   // inner_mail_box_->Subscrdibe(
   //    typeid(std::tuple<std::shared_ptr<CTPOrderField>>), this);
   inner_mail_box_->Subscribe(typeid(std::tuple<InputOrder>), this);
